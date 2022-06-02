1c71ffff14292b8ebfeae37a67de862b82c10f32
Shashank Krishna Vempati <vempatishashankbzy@gmail.com>
2022-06-02 17:03:02 +0530

    Create Best Time to Buy and Sell Stock.py

---
+++ Best Time to Buy and Sell Stock.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        low=sys.maxsize
+        diff=0
+        for i in range(len(prices)):
+            if prices[i]<low:
+                low=prices[i]
+            if i!=0 and prices[i]>prices[i-1]:
+                temp=prices[i]-low
+                if temp>diff:
+                    diff=temp
+        return diff
